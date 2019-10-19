f50159de8f1a45a787ab59622fec197b93e2bc39
Corey Bastarache <corey.bastarache@hotmail.com>
2019-10-19 16:09:52 -0400

    web things

--- webpage/app/views.py
+++ webpage/app/views.py
@@ -17,11 +17,30 @@ def greenhouses(request):
 def greenhouse(request, id):
     greenhouse = Greenhouse.objects.get(id=id)
     certifications = Certification.objects.filter(greenhouse = greenhouse.id).values()
-    products = Unit.objects.filter(greenhouse = greenhouse.id).values()
-    print(certifications)
+    db_products = Unit.objects.filter(greenhouse = greenhouse.id)
+
+    products = []
+    for product in db_products:
+        products += [{
+            'product' : product,
+            'recipe': product.plantRecipe,
+            }]
+        print(product.id)
+        print(product.plantRecipe)
+        #plantName = PlantRecipe.objects.get(id = product.plantRecipe)
+        #print(plantName)
+
     context = {
         'greenhouse': greenhouse,
         'certifications': certifications,
         'products': products,
     }
     return render(request, 'greenhouse.html', context)
+
+def product(request, id):
+    product = Unit.objects.get(id=id)
+    print(product.plantRecipe.name)
+    context = {
+        'product': product,
+    }
+    return render(request, 'product.html', context)
